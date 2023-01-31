b334e829a5fd96b25c44cbaa97e1db8803697c70
Ivan Aires <ivanaires32@gmail.com>
2023-01-31 14:41:51 -0300

    montar blusa check

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 const usuario = prompt("Qual é seu nome?");
 
-function selecionarOpcao(opcao) {
+function selecionarModelo(opcao) {
     const jaSelecionado = document.querySelector('.modelo .selecionado');
     if (jaSelecionado !== null) {
-        jaSelecionado.classList.add('naoSelecionado')
         jaSelecionado.classList.remove('selecionado')
     }
-    opcao.classList.remove('naoSelecionado')
+    opcao.classList.add('selecionado')
+}
+
+function selecionarGola(opcao) {
+    const jaSelecionado = document.querySelector('.gola .selecionado');
+    if (jaSelecionado !== null) {
+        jaSelecionado.classList.remove('selecionado')
+    }
+    opcao.classList.add('selecionado')
+}
+
+function selecionarTecido(opcao) {
+    const jaSelecionado = document.querySelector('.tecido .selecionado');
+    if (jaSelecionado !== null) {
+        jaSelecionado.classList.remove('selecionado')
+    }
     opcao.classList.add('selecionado')
 }
